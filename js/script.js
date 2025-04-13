908b660ea34be3ef98b02a0191b134bb92fd593e
kirsten111102 <kirsten11112002@gmail.com>
2025-04-13 22:34:14 +0700

    Basic first look of Hellbound Website

---
+++ js/script.js
@@ -0,0 +1,11 @@
+fetch('/html/header.html')
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+    });
+
+fetch('/html/footer.html')
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    });
