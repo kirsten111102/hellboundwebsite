d7cf44e29a8f54b7d69ea3e3a9d6f1e00e433e3a
kirsten111102 <kirsten11112002@gmail.com>
2025-05-02 21:12:59 +0700

    Adapt all Transfer Information of all teams and header with database

--- js/database/showdb.js
+++ js/database/showdb.js
@@ -6,7 +6,7 @@ async function showEsportsHomeTeam() {
         esportsteam.forEach(team => {
             const a = document.createElement("a");
             a.id = team.id;
-            a.href = `teams/${team.id}.html`;
+            a.href = `/html/teams/${team.id}.html`;
             a.innerHTML = `
                 <div class="game">
                     <div><img src="/images/games/${team.id}.png" class="avtgame"></div>
@@ -20,33 +20,41 @@ async function showEsportsHomeTeam() {
 
 function showEsportsHeaderTeam() {
     db.esportsteam.toArray().then(esportsteam => {
-        const list = document.getElementById("teams");
-        list.innerHTML = "";
+        const teamlink = document.getElementById("link_teams");
+        teamlink.innerHTML = "";
 
         esportsteam.forEach(team => {
-            const li = document.createElement("li");
-            li.textContent = `${team.name}`;
-            list.appendChild(li);
+            const a = document.createElement("a");
+            a.href = `/html/teams/${team.id}.html`;
+            a.innerHTML = `<li>${team.name}</li>`;
+            teamlink.appendChild(a);
         });
     });
 }
 
-function showTransferNews() {
-    db.transfer_news.toArray().then(transfer_news => {
-        const table = document.getElementById("news");
-        table.innerHTML = "";
+async function showTransferNews(bodyId){
+    try {
+        const transfer_news = await db.transfer_news
+        .where('team_id')
+        .equals(bodyId)
+        .toArray();
+
+        const news = document.getElementById("news");
+        news.innerHTML = "";
 
         transfer_news.forEach(transfer_new => {
-            const row = document.createElement("tr");
-            row.innerHTML = `
-                <td>${transfer_new.news}</td>
-                <td class="focus">${transfer_new.time}</td>
-            `;
-            table.appendChild(row);
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${transfer_new.news}</td>
+            <td class="focus">${transfer_new.time}</td>
+        `;
+        news.appendChild(row);
         });
-    });
+    } catch (error) {
+        console.error("Error loading transfer news:", error);
+    }
 }
 
 
 document.addEventListener("DOMContentLoaded", showEsportsHomeTeam);
-showTransferNews();
+document.addEventListener("DOMContentLoaded", showEsportsHeaderTeam);
