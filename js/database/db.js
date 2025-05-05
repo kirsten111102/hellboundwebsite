0b405f11d7bdc48afe29bf6911b2c0eff925b5b7
kirsten111102 <kirsten11112002@gmail.com>
2025-05-05 19:05:42 +0700

    Adding Position and Player table to the database and adapt it in the web

--- js/database/db.js
+++ js/database/db.js
@@ -2,7 +2,9 @@ const db = new Dexie("HellboundEsports");
 
 db.version(1).stores({
     esportsteam: "id, name",
-    transfer_news: "id, news, time, team_id"
+    transfer_news: "id, news, time, team_id",
+    position: "id, pos, team_id",
+    player: "++id, name, team, age, pos, status",
 });
 
 window.db = db;
---
+++ js/database/player.js
@@ -0,0 +1,3 @@
+async function deletePlayers(){
+    db.player.clear();
+}
